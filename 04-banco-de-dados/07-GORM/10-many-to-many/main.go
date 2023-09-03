5874eb719752c6c1c6022c03791aa1dd015c1e4f
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-03 18:56:20 -0300

    Many to many

---
+++ 04-banco-de-dados/07-GORM/10-many-to-many/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product `gorm:"many2many:product_categories;"`
+}
+
+type Product struct {
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	Categories   []Category `gorm:"many2many:product_categories;"`
+	gorm.Model
+}
+
+// aqui teremos que ter dado drop na tabela antes de executar o código
+func main() {
+	dsn := "root:root@tcp(192.168.157.51:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	// category := Category{Name: "Categoria 1"}
+	// db.Create(&category)
+
+	// category2 := Category{Name: "Categoria 2"}
+	// db.Create(&category2)
+
+	// product := Product{
+	// 	Name:       "Produto 1",
+	// 	Price:      10.0,
+	// 	Categories: []Category{category, category2},
+	// }
+	// db.Create(&product)
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		fmt.Println(category.Name, ":")
+		for _, product := range category.Products {
+			println("- ", product.Name)
+		}
+	}
+}
